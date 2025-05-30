7d2ff67e3323a54391fb8ceb0b2b831c9e564bad
Dex-code61 <deodatkanabo@gmail.com>
2025-05-30 17:21:05 +0000

    debut

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full min-h-screen bg-background dark">
+      <h1 className="text-blue-500 text-6xl">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
